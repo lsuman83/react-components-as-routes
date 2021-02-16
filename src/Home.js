119f846e92630dc96a16270ff7c0599898b00b2c
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-02-16 16:26:53 -0500

    Done.

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Home extends from React.Component {
+    render() {
+        return (
+            <h1>Home!</h1>
+        )
+    }
+}
+
+
+export default Home;
